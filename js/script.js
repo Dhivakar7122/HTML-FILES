8e6fa723c6c9683ad93e7cff1c3a6c44d61fa889
Dhivakar7122 <dhivakarkaruda@gmail.com>
2024-02-13 11:41:14 -0800

    add

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let str1 ='Strive not Success';
+let str2 = 'But rather to be a value';
+// console.log(str.concat(str1,str2));
+// console.log(str2.slice(4));
+// console.log(str2.substring(2,9));
+// console.log(str2.substr(4,10));(length)
+// console.log(str2.replace('but','which'))replace
+let names =['karrthi,sundar,priya,ram'];
+console.log(names[1],names[2]);
